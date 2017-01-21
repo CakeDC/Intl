adfdff6239410630f5b2e41899bddd9ce4a157ab
Larry E. Masters <phpnut@gmail.com>
2017-01-21 14:30:52 -0600

    Adding compatability file Adding composer.json

---
+++ src/intl_compat.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright 2017, Cake Development Corporation (http://cakedc.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright 2017, Cake Development Corporation (http://cakedc.com)
+ * @link https://www.cakedc.com
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+class_alias('CakeDC\Intl\MessageFormatter', 'MessageFormatter');
