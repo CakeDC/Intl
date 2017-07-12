b31fff5638c23aa9425e4aa57d07e3b66dcb8f55
Larry E. Masters <phpnut@gmail.com>
2017-07-12 13:57:31 -0500

    FIxing NameSpace.

--- src/Utility/PatternParser.php
+++ src/Utility/PatternParser.php
@@ -10,7 +10,7 @@
  * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
-namespace CakeDC\intl\src\Utility;
+namespace CakeDC\Intl\Utility;
 
 
 class PatternParser
@@ -227,6 +227,112 @@ class PatternParser
     {
 
     }
-
+    /*
+    Timestamp:    1499821968
+    G => AD
+    GG => AD
+    GGG => AD
+    GGGG => Anno Domini
+    GGGGG => A
+    yy => 17
+    y => 2017
+    yyyy => 2017
+    u => 2017
+    U => 2017
+    r => 2017
+    Q => 3
+    QQ => 03
+    QQQ => Q3
+    QQQQ => 3rd quarter
+    QQQQQ => 3rd quarter
+    q => 3
+    qq => 03
+    qqq => Q3
+    qqqq => 3rd quarter
+    qqqqq => 3rd quarter
+    M => 7
+    MM => 07
+    MMM => Jul
+    MMMM => July
+    MMMMM => J
+    n =>
+    m => 12
+    M => 7
+    F => 2
+    LLLLL => J
+    w => 28
+    ww => 28
+    W => 3
+    d => 11
+    dd => 11
+    D => 192
+    F => 2
+    g => 2457946
+    E => Tue
+    EE => Tue
+    EEE => Tue
+    EEEE => Tuesday
+    EEEEE => T
+    EEEEEE => Tu
+    e => 3
+    ee => 03
+    eee => Tue
+    eeee => Tuesday
+    eeeee => T
+    eeeeee => Tu
+    c => 3
+    cc => 3
+    ccc => Tue
+    cccc => Tuesday
+    ccccc => T
+    cccccc => Tu
+    a => PM
+    h => 8
+    hh => 08
+    H => 20
+    HH => 20
+    k => 20
+    kk => 20
+    K => 8
+    KK => 08
+    m => 12
+    mm => 12
+    s => 48
+    ss => 48
+    S => 0
+    SS => 00
+    SSS => 000
+    SSSS => 0000
+    A => 72768000
+    z => CDT
+    zz => CDT
+    zzz => CDT
+    zzzz => Central Daylight Time
+    Z => -0500
+    ZZ => -0500
+    ZZZ => -0500
+    ZZZZ => GMT-05:00
+    OOOO => GMT-05:00
+    VVVV => Chicago Time
+    vvvv => Central Time
+    ZZZZZ => -05:00
+    XXXXX => -05:00
+    O => GMT-5
+    OOOO => GMT-05:00
+    v => CT
+    VV => America/Chicago
+    VVV => Chicago
+    X => -05
+    XX => -0500
+    XXX => -05:00
+    XXXX => -0500
+    XXXXX => -05:00
+    x => -05
+    xx => -0500
+    xxx => -05:00
+    xxxx => -0500
+    xxxxx => -05:00
+    MMMM dd, yyyy h:m a => July 11, 2017 8:12 PM
+    */
 
 }
